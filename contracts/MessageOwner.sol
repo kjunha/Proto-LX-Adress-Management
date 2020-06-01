86dd3e96d47de590870db2132485ffd56413702a
kjunha <kjunha77@gmail.com>
2020-06-01 11:43:16 +0900

    fix to non-token version app

--- contracts/MessageOwner.sol
+++ contracts/MessageOwner.sol
@@ -1,11 +1,21 @@
 pragma solidity ^0.5.0;
 
 contract MessageOwner {
-    string public regId;
-    constructor(string memory _regId) public {
-        regId = _regId;
+    string public name;
+    string public residence;
+    mapping(address => bool) accessability;
+    constructor(string memory _name, string memory _residence) public {
+        name = _name;
+        residence = _residence;
     }
-    function getRegId() public view returns(string memory) {
-        return regId;
+    function getInfo() public view returns(string memory _name, string memory _residence) {
+        _name = name;
+        _residence = residence;
+    }
+    function register(address _corpId) public {
+        accessability[_corpId] = true;
+    }
+    function deregister(address _corpId) public {
+        accessability[_corpId] = false;
     }
 }
